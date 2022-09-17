2f439553fdc243dd7564420c69087472b982f0f0
Dante <jazzy.d1302@outlook.com>
2022-09-17 19:44:41 -0400

    working?

--- src/orquestra/quantum/backends/PauliSandwichBackend.py
+++ src/orquestra/quantum/backends/PauliSandwichBackend.py
@@ -21,20 +21,20 @@ class PauliSandwichBackend(QuantumBackend):
             # print("U: ",type(self.U))
             # print("Op: ",type(operation))
             if operation.gate is self.U.gate:
-                # print("Sandwich U")
+                print("Sandwich U")
                 for P in self.bread_gates:
                     # print("dagger: ",type(self.U.gate.dagger(*op_indices)))
                     n_sandwiches += 1
                     op_indices = operation.qubit_indices
-                    control_qubit_index = circuit.n_qubits + n_sandwiches -1
-                    controlled_P_qubits = (control_qubit_index,) + data_qubit_indices
                     # sandwich U between controlled operations
                     # breakpoint()
-                    # Pprime = self.U.gate(*op_indices).lifted_matrix(control_qubit_index) * P.lifted_matrix(control_qubit_index) * self.U.gate.dagger(*op_indices).lifted_matrix(control_qubit_index)
+                   
                     new_circuit +=  self.U.gate.dagger(*op_indices)
                     new_circuit += P
                     new_circuit +=  self.U.gate(*op_indices)
+                    # ic(new_circuit)
                     new_circuit.to_unitary()
+                    ic(new_circuit)
                     new_circuit += operation
             else:
                 new_circuit += operation
@@ -45,4 +45,4 @@ class PauliSandwichBackend(QuantumBackend):
             for key in raw_counts.keys():
                 if "1" not in key[circuit.n_qubits:]:
                     sandwiched_counts[key[:circuit.n_qubits]] = raw_counts[key]
-        return Measurements.from_counts(sandwiched_counts)
+        return new_circuit
--- src/orquestra/quantum/backends/Test.py
+++ src/orquestra/quantum/backends/Test.py
@@ -1,6 +1,6 @@
 
 from orquestra.quantum.circuits._gates import GateOperation
-from orquestra.quantum.circuits import CNOT, H, Circuit, X, Z
+from orquestra.quantum.circuits import CNOT, H, Circuit, X, Z, Y
 from icecream import ic
 from orquestra.quantum.symbolic_simulator import SymbolicSimulator
 from orquestra.integrations.qiskit.simulator import QiskitSimulator
@@ -17,14 +17,9 @@ from PauliSandwichBackend import PauliSandwichBackend
 bell_circuit = Circuit()
 bell_circuit += H(0)
 bell_circuit += CNOT(0, 1)
+bell_circuit += X(1)
 
-Array_circuit = Circuit()
 
-Array_circuit += Z(1)
-Array_circuit += Y(1)
-Array_circuit += Z(1)
-Array_circuit += Z(1)
-Array_circuit += X(1)
 
 
 
@@ -50,13 +45,12 @@ print('\n\n\n')
 
 sym_simulator = SymbolicSimulator()
 
-Circuit([])
-tester=PauliSandwichBackend(GateOperation(bell_circuit.operations[0].gate, 0),bell_circuit.operations,sym_simulator)
-measurements = tester.run_circuit_and_measure(bell_circuit,num_samples)
-ic(measurements.get_counts())
+Array_circuit = Circuit([X(1),Z(1),X(1)])
+tester=PauliSandwichBackend(GateOperation(bell_circuit.operations[2].gate, 0),Array_circuit.operations,sym_simulator)
+new_circuit = tester.run_circuit_and_measure(bell_circuit,num_samples)
 
 
-qiskit_circuit = export_to_qiskit(bell_circuit)
+qiskit_circuit = export_to_qiskit(new_circuit)
 qiskit_circuit.x(1)
 from orquestra.integrations.cirq.conversions._circuit_conversions import export_to_cirq
 
