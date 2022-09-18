20391bb58242b7f121ed0c55129e23ca4ab77229
Dante <jazzy.d1302@outlook.com>
2022-09-18 00:42:15 -0400

    functional program

--- src/orquestra/quantum/backends/PauliSandwichBackend.py
+++ src/orquestra/quantum/backends/PauliSandwichBackend.py
@@ -1,7 +1,4 @@
-from orquestra.quantum.circuits._gates import GateOperation
-from orquestra.quantum.circuits import CNOT, H, Circuit
-from icecream import ic
-from orquestra.integrations.qiskit.backend import QiskitBackend
+from orquestra.quantum.circuits import Circuit
 from orquestra.quantum.api.backend import QuantumBackend
 from orquestra.quantum.measurements import Measurements
 
@@ -18,31 +15,25 @@ class PauliSandwichBackend(QuantumBackend):
         n_sandwiches = 0
         # create and run sandwiched circuit
         for operation in circuit.operations:
-            # print("U: ",type(self.U))
-            # print("Op: ",type(operation))
             if operation.gate is self.U.gate:
-                print("Sandwich U")
                 for P in self.bread_gates:
-                    # print("dagger: ",type(self.U.gate.dagger(*op_indices)))
                     n_sandwiches += 1
                     op_indices = operation.qubit_indices
-                    # sandwich U between controlled operations
-                    # breakpoint()
-                   
+                    control_qubit_index = circuit.n_qubits + n_sandwiches
+                    controlled_P_qubits = (control_qubit_index,) + data_qubit_indices
+
                     new_circuit +=  self.U.gate.dagger(*op_indices)
                     new_circuit += P
                     new_circuit +=  self.U.gate(*op_indices)
-                    # ic(new_circuit)
-                    new_circuit.to_unitary()
-                    ic(new_circuit)
+
                     new_circuit += operation
             else:
                 new_circuit += operation
-            raw_meas = self.inner_backend.run_circuit_and_measure(new_circuit, n_samples)
+        raw_meas = self.inner_backend.run_circuit_and_measure(new_circuit, n_samples)
             # eliminate runs in which an error was caught
-            raw_counts = raw_meas.get_counts() # get dictionary of outputs
-            sandwiched_counts = {}
-            for key in raw_counts.keys():
-                if "1" not in key[circuit.n_qubits:]:
-                    sandwiched_counts[key[:circuit.n_qubits]] = raw_counts[key]
-        return new_circuit
+        raw_counts = raw_meas.get_counts() # get dictionary of outputs
+        sandwiched_counts = {}
+        for key in raw_counts.keys():
+            if "1" not in key[circuit.n_qubits:]:
+                sandwiched_counts[key[:circuit.n_qubits]] = raw_counts[key]
+        return Measurements.from_counts(sandwiched_counts)
--- src/orquestra/quantum/backends/Test.py
+++ src/orquestra/quantum/backends/Test.py
@@ -1,4 +1,5 @@
 
+from cirq import measure
 from orquestra.quantum.circuits._gates import GateOperation
 from orquestra.quantum.circuits import CNOT, H, Circuit, X, Z, Y
 from icecream import ic
@@ -10,57 +11,26 @@ from orquestra.integrations.qiskit.conversions import (
     export_to_qiskit,
     import_from_qiskit,
 )
+import qiskit.providers.aer.noise as noise
+from orquestra.integrations.qiskit.simulator import QiskitSimulator
+from orquestra.quantum.circuits import CNOT, Circuit, X
 
 
 from PauliSandwichBackend import PauliSandwichBackend
 
-bell_circuit = Circuit()
-bell_circuit += H(0)
-bell_circuit += CNOT(0, 1)
-bell_circuit += X(1)
+bell_circuit = Circuit([CNOT(0, 1), X(0), X(1), CNOT(1, 2)])
 
+noise_model = noise.NoiseModel()
+error = noise.depolarizing_error(0.1, 2)
+noise_model.add_all_qubit_quantum_error(error, ["cx"])
 
+qiskit_sim = QiskitSimulator(device_name="aer_simulator", noise_model=noise_model)
+noisey_measurements = qiskit_sim.run_circuit_and_measure(bell_circuit, 1000)
 
+Array_circuit = [Z(0),X(0),Z(0)]
+for i in range(2):
+    tester=PauliSandwichBackend(GateOperation(bell_circuit.operations[0].gate, i),Array_circuit, qiskit_sim)
+measurements = tester.run_circuit_and_measure(bell_circuit,1000)
 
-
-sym_simulator = SymbolicSimulator()
-num_samples = 1000
-measurements2 = sym_simulator.run_circuit_and_measure(bell_circuit, num_samples)
-ic(measurements2.get_counts())
-
-qiskit_circuit = export_to_qiskit(bell_circuit)
-qiskit_circuit.x(1)
-from orquestra.integrations.cirq.conversions._circuit_conversions import export_to_cirq
-
-bell_circuit_X = import_from_qiskit(qiskit_circuit)
-
-cirq_circuit = export_to_cirq(bell_circuit_X)
-print(cirq_circuit)
-
-sv_simulator = QiskitSimulator("aer_simulator_statevector")
-wavefunction = sv_simulator.get_wavefunction(bell_circuit_X)
-ic(wavefunction.amplitudes)
-
-print('\n\n\n')
-
-sym_simulator = SymbolicSimulator()
-
-Array_circuit = Circuit([X(1),Z(1),X(1)])
-tester=PauliSandwichBackend(GateOperation(bell_circuit.operations[2].gate, 0),Array_circuit.operations,sym_simulator)
-new_circuit = tester.run_circuit_and_measure(bell_circuit,num_samples)
-
-
-qiskit_circuit = export_to_qiskit(new_circuit)
-qiskit_circuit.x(1)
-from orquestra.integrations.cirq.conversions._circuit_conversions import export_to_cirq
-
-bell_circuit_X = import_from_qiskit(qiskit_circuit)
-cirq_circuit = export_to_cirq(bell_circuit_X)
-print(cirq_circuit)
-sv_simulator = QiskitSimulator("aer_simulator_statevector")
-wavefunction = sv_simulator.get_wavefunction(bell_circuit_X)
-ic(wavefunction.amplitudes)
-
-
-
-
+ic(noisey_measurements.get_counts())
+ic(measurements.get_counts())
